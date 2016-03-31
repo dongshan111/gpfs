e2edc0dbb3bf15fb4b2dc3ad817e143361c4cce7
dongshan111 <dongshan111@126.com>
2016-03-31 14:23:38 +0800

    autoinstall ok

--- autoinstall.sh
+++ autoinstall.sh
@@ -6,7 +6,7 @@ yum -y  install make
 rpm -ivh  $INSTALL_PACKAGE/gpfs.base-4.1.1-1.x86_64.rpm
 rpm -ivh  $INSTALL_PACKAGE/gpfs.gpl-4.1.1-1.noarch.rpm
 rpm -ivh  $INSTALL_PACKAGE/gpfs.msg.en_US-4.1.1-1.noarch.rpm
-#rpm -ivh  $INSTALL_PACKAGE/gpfs.gskit-8.0.50-40.x86_64.rpm
+rpm -ivh  $INSTALL_PACKAGE/gpfs.gskit-8.0.50-40.x86_64.rpm
 rpm -ivh  $INSTALL_PACKAGE/gpfs.ext-4.1.1-1.x86_64.rpm
 rpm -ivh  $INSTALL_PACKAGE/gpfs.docs-4.1.1-1.noarch.rpm
 rpm -ivh  $INSTALL_PACKAGE/gpfs.hadoop-connector-2.7.0-3.x86_64.rpm
